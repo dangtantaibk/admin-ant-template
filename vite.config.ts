973d0afd094ff872d0c18e524e43b87b22e23b10
TaiDang <tai.dt@kdc.vn>
2025-05-15 17:16:18 +0700

    feat: Set server port in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3101,
     proxy: {
       '/api': {
         target: 'https://api.yensao24h.com',
