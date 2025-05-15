c236f0bee67f98fe52759f3e65c6ad1ff9ee9bed
TaiDang <tai.dt@kdc.vn>
2025-05-15 21:45:40 +0700

    feat: update ecosystem configuration and remove server.js file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,18 +1,14 @@
 module.exports = {
-    apps: [
-      {
-        name: "admin-ant-template",
-        script: "./server.js",
-        instances: "max",
-        exec_mode: "cluster",
-        env: {
-          NODE_ENV: "production",
-          PORT: 3101
-        },
-        env_production: {
-          NODE_ENV: "production",
-          PORT: 80
-        }
+    apps: [{
+      name: 'admin-ant-template',
+      script: 'npm',
+      args: 'run preview',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production'
       }
-    ]
+    }]
   };
